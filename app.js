9d57e19c844f1b18c432138a333874b39f796a5f
WaterMan-Joy <nietzsche21st@gmail.com>
2022-11-10 15:02:08 +0900

    second'

---
+++ app.js
@@ -0,0 +1,8 @@
+var userInput;
+var userName;
+userInput = 10;
+console.log(userInput);
+userInput = "Joy";
+console.log(userInput);
+userInput = userName;
+console.log(userInput);
---
+++ app.ts
@@ -0,0 +1,11 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 10;
+console.log(userInput);
+userInput = "Joy";
+console.log(userInput);
+
+// userInput = userName;
+// Variable 'userName' is used before being assigned.ts(2454)
+// console.log(userInput);
