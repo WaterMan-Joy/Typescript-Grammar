1d32eb05ea731f26ee092dc6666c1a98ee939c7b
WaterMan-Joy <nietzsche21st@gmail.com>
2022-11-18 14:42:01 +0900

    generic

--- dist/generic.js
+++ dist/generic.js
@@ -3,10 +3,24 @@ const genericArray = [];
 const genericArray2 = [];
 const genericArray3 = [];
 function merge(objA, objB) {
-    return Object.assign({}, objA, objB);
+    return Object.assign(objA, objB);
 }
-const merge1 = merge({ name: "Joy" }, { age: "30" });
-const merge2 = merge({ hobbies: ["swim", "coding"] }, { age: "30" });
+const merge1 = merge({ name: "Joy" }, { age: 30 });
+const merge2 = merge({ hobbies: ["swim", "coding"] }, { age: 30 });
+console.log(merge1);
+console.log(merge2);
 console.log(merge1.name);
+console.log(merge1.age);
 console.log(merge2.hobbies[1]);
+console.log(merge2.age);
+console.log("===========");
+function countAndDescription(element) {
+    let descriptionText = "no value";
+    if (element.length > 0) {
+        descriptionText = element.length + " - count";
+    }
+    return [element, descriptionText];
+}
+const c1 = countAndDescription("gigi");
+console.log(c1);
 //# sourceMappingURL=generic.js.map
--- src/generic.ts
+++ src/generic.ts
@@ -13,12 +13,38 @@ const genericArray3: string[] = [];
 //   data.split(" ");
 // });
 
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 // (method) ObjectConstructor.assign<T extends {}, U>(target: T, source: U): T & U (+3 overloads)
 
-const merge1 = merge({ name: "Joy" }, { age: "30" });
-const merge2 = merge({ hobbies: ["swim", "coding"] }, { age: "30" });
+const merge1 = merge<{ name: string }, { age: number }>(
+  { name: "Joy" },
+  { age: 30 }
+);
+const merge2 = merge<{ hobbies: string[] }, { age: number }>(
+  { hobbies: ["swim", "coding"] },
+  { age: 30 }
+);
+
+console.log(merge1);
+console.log(merge2);
 console.log(merge1.name);
+console.log(merge1.age);
 console.log(merge2.hobbies[1]);
+console.log(merge2.age);
+console.log("===========");
+
+interface lengthy {
+  length: number;
+}
+
+function countAndDescription<T extends lengthy>(element: T): [T, string] {
+  let descriptionText = "no value";
+  if (element.length > 0) {
+    descriptionText = element.length + " - count";
+  }
+  return [element, descriptionText];
+}
+const c1 = countAndDescription("gigi");
+console.log(c1);
